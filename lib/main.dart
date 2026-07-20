a9a7bfba9740c98dbcec67bdb98a400b5da6389c
Z7402 <81816703+Z7402@users.noreply.github.com>
2026-07-20 12:00:37 +0800

    Initialize Chinese date formatting at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'screen_splash.dart';
 import 'provider_theme.dart';
 import 'provider_wallpaper.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('zh_CN');
   runApp(
     const ProviderScope(
       child: BananachockApp(),
