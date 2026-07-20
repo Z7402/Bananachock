ad4728483880474782283542a312a6aad2397f52
Z7402 <Z7402@users.noreply.github.com>
2026-07-20 11:29:40 +0800

    Fix widget test ProviderScope setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,14 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:bananachock/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('App should show the splash screen', (WidgetTester tester) async {
-    await tester.pumpWidget(const BananachockApp());
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: BananachockApp(),
+      ),
+    );
     await tester.pump();
 
     expect(find.text('Bananachock'), findsOneWidget);
